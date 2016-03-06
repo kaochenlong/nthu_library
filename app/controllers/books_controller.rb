1b6c58964b59936aa51c9a7a960d53c121fac928
Eddie Kao <eddie@digik.com.tw>
2016-03-06 14:56:19 +0800

    add kaminari

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   def index
-    @books = Book.all
+    @books = Book.page(params[:page]).per(2)
   end
 
   def show
